2e6aca595a5dcb95b52f610023dfe19222c6ac3b
HIlse <trandat.arc@gmail.com>
2020-10-03 19:03:04 +0700

    try canvan full height but not work

--- js/Main.js
+++ js/Main.js
@@ -8,20 +8,23 @@ var touchGround = false;
 
 function appOnLoad()
 {
-    
+
     canvas = document.getElementById("game_canvas");
+    canvas.height = window.innerHeight;
+    canvas.width = canvas.height/2;
+
     ctx = canvas.getContext("2d");
     gGame = new Game();
     gGame.init();
-    
+
     canvas.addEventListener("mousedown", function(){clicked = true;});
     canvas.addEventListener("mouseup", function(){clicked = false;});
     canvas.addEventListener("touchstart", function(){clicked = true;});
     canvas.addEventListener("touchcancel", function(){clicked = false;});
-    
-    
-    
-    
+
+
+
+
     setInterval(loop, 16);
 }
 
